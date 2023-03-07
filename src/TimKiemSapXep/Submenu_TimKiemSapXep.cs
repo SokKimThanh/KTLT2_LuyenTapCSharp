c5013aa998e918734c5b2f56d6ceb89d641767b6
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-03-07 22:09:39 +0700

    update 2 thuat toan tim kiem

--- src/TimKiemSapXep/Submenu_TimKiemSapXep.cs
+++ src/TimKiemSapXep/Submenu_TimKiemSapXep.cs
@@ -4,52 +4,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-namespace ThuatToan
+namespace KTLT2_TAODOITUONG
 {
     class SubMenu_TimKiemSapXep
     {
         internal void GetSubMenu()
         {
+            // khai bao
             Random d = new Random();
-            WriteLine("Vui long nhap co bao nhieu phan tu ma ban muon nhap?");
-            int n;
-            int.TryParse(ReadLine(), out n);
+            int n = 0;
+            int key;
+            WriteLine("BAI TAP O LOP: ");
+            WriteLine("\n---------------------------------------------------------------------\n");
+            WriteLine("CHUONG TRINH LINEAR SEARCH CHUA DUOC SAP XEP");
+            // kiem tra nhap
+            do
+            {
+                Write("Nhap n: ");
+            } while (int.TryParse(ReadLine(), out n) == false);
+
+
+            WriteLine("Ban nhap " + n + " so tu dong");
 
             // nhap 
-            int[] arr = Utililies.NhapRandomMangSoNguyen(n, d);
+            // int[] arr = Utililies.NhapRandomMangSoNguyen(n, d);
+            int[] arr = { 0, -1, 8, 8, 8, 8, -1, 8 };
             // xuat
-            Utililies.Xuat(arr.ToArray());
-
-            WriteLine("Vui long nhap key ban muon tim? ");
-            int key = int.Parse(ReadLine());
+            Utililies.Xuat(arr);
+            WriteLine();
+            // nhap key thuat toan chua sap xep unoder linear search
+            WriteLine("---------------------------------------------------------------------");
+            WriteLine("Thuat toan linear search(chua sap xep)".ToUpper());
+            Write("Vui long nhap key: ");
+            int.TryParse(ReadLine(), out key);
 
             // unorder linear search
-            int ketquaUnorderLNS = Utililies.UnorderLinearSearch(arr, key);
-            WriteLine("{0, -10} {1,-10}", "Vi tri unorder linear search", ketquaUnorderLNS);
+            int vitriDauTien = Utililies.UnorderLinearSearch(arr, key);
+            // in ket qua
+            if (vitriDauTien != -1)
+            {
+                WriteLine("Tim thay key = " + key + " tai vi tri: " + vitriDauTien);
+            }
+            else
+            {
+                WriteLine("Khong Tim thay key = " + key + " tai bat ky vi tri nao.");
+            }
 
-            WriteLine("\nVui long nhap key ban muon tim? ");
-            key = int.Parse(ReadLine());
 
-            //sort list
-            WriteLine("\n Sap xep mang\n");
-            Array.Sort(arr);
-            //Array.Reverse(arr); dao nguoc sap xep giam dan
-            Utililies.Xuat(arr);
+            WriteLine("\n\n---------------------------------------------------------------------");
+            WriteLine("CAI TIEN CHUONG TRINH \nLINEAR SEARCH TIM KEY TAI TAT CA VI TRI");
+            int[] arrIndex = Utililies.TimTatCaViTriX(arr, key);
+            if (arrIndex.Length != 0)
+            {
+                WriteLine("\nDanh sach index xuat hien la: ");
+                Utililies.Xuat(arrIndex.ToArray());
+            }
+            WriteLine("\n");
+            WriteLine("\n\n---------------------------------------------------------------------");
 
-            // order linear search
-            int ketquaOrderLNS = Utililies.OrderLinearSearch(arr, key);
 
-            // Binary search
-            // sort list        
-            int ketquaBS = Utililies.BinarySearch(arr, key);
+            WriteLine("\n\n---------------------------------------------------------------------");
+            WriteLine("XOAT TAT CA VI TRI KEY");
+            int[] arrRemovedIndex = arr;
+            Utililies.XoaTatCa(ref arrRemovedIndex, key);
+            if (arrIndex.Length != 0)
+            {
+                WriteLine("\nDanh sach sau khi xoa: ");
+                Utililies.Xuat(arrRemovedIndex);
+            }
+            WriteLine("\n");
+            WriteLine("\n\n---------------------------------------------------------------------");
+            //WriteLine("\n\n---------------------------------------------------------------------");
+            //WriteLine("CAI TIEN CHUONG TRINH LINEAR SEARCH BANG PHUONG PHAP SAP XEP TRUOC KHI TIM KIEM");
 
-            //in ket qua 
-            WriteLine("{0, -10} {1,-10}", "Vi tri order linear search", ketquaOrderLNS);
-            WriteLine("{0, -10} {1,-10}", "Vi tri binary search", ketquaBS);
+            ////sort list
+            //WriteLine("\n  CHUONG TRINH DANG SAP XEP DANH SACH....\n");
+            //Array.Sort(arr);
+            //WriteLine("\n Danh SACH SAU KHI SAP XEP LA: \n");
+            //Utililies.Xuat(arr);
 
+            ////VA BAT DAU TIM KIEM CAI TIEN LINEAR SEARCH...
+            //WriteLine("\nVui long nhap key ban muon tim? ");
+            //key = int.Parse(ReadLine());
+
+            //// order linear search
+            //int ketquaOrderLNS = Utililies.OrderLinearSearch(arr, key);
+
+            //WriteLine("--------------------------------------------------------------------------------");
+            //WriteLine("CAI TIEN THUAT TOAN KIEM KIEM BANG THUAT TOAN BINARY SEARCH( DANH SACH CHUA DUOC ");
+
+            //// Binary search
+            //// sort list        
+            //int ketquaBS = Utililies.BinarySearch(arr, key);
+
+            ////in ket qua 
+            //WriteLine("{0, -10} {1,-10}", "Vi tri order linear search", ketquaOrderLNS);
+            //WriteLine("{0, -10} {1,-10}", "Vi tri binary search", ketquaBS);
 
             //int ketquaInscreseBS = Utililies.IncreaseOrderBinarySearch(arr, key);
 
+            //// dao nguoc sap xep giam dan
+            //Array.Reverse(arr);
+            //int ketquaDecreaseBS = Utililies.DecreaseOrderBinarySearch(arr, key);
 
             WriteLine("Press any key to exit.");
             ReadKey();
--- src/TimKiemSapXep/Utililies.cs
+++ src/TimKiemSapXep/Utililies.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-namespace ThuatToan
+namespace KTLT2_TAODOITUONG
 {
     public class Utililies
     {
@@ -27,20 +27,55 @@ namespace ThuatToan
         }
         public static int UnorderLinearSearch(int[] arr, int key)
         {
-            int kqCuoiCung = 0;
             for (int i = 0; i < arr.Length; i++)
             {
                 if (arr[i] == key)
                 {
-                    kqCuoiCung = i;
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static int[] TimTatCaViTriX(int[] arr, int key)
+        {
+            // khai bao mang rong chua co phan tu nao
+            List<int> arrIndex = new List<int>();
+            bool check = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    arrIndex.Add(i);
+                    check = true;
+                }
+            }
+            if (check == false)
+            {
+                Write($"Khong tim thay {key}");
+            }
+            return arrIndex.ToArray();
+        }
+
+        public static void XoaTatCa(ref int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length;)
+            {
+                // tim key
+                if (arr[i] == key)
+                {
+                    for (int j = i; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    Array.Resize(ref arr, arr.Length - 1);
                 }
                 else
                 {
-                    kqCuoiCung = -1;
+                    i++;
                 }
             }
-            return kqCuoiCung;
         }
+
         public static int OrderLinearSearch(int[] arr, int key)
         {
             int kqCuoiCung = 0;
@@ -70,9 +105,40 @@ namespace ThuatToan
             return arr;
         }
 
-        internal static int IncreaseOrderBinarySearch(int[] arr, int key)
+        internal static int IncreaseOrderBinarySearch(int[] arrInt, int key)
         {
-            throw new NotImplementedException();
+            // xuat ket qua
+            int left = 0;
+            int right = arrInt.Length - 1;
+            int mid;
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+                if (arrInt[mid] == key)
+                    return mid;
+                else if (arrInt[mid] < key)
+                    left = mid + 1;
+                else right = mid - 1;
+            }
+            return -1;
+        }
+
+        internal static int DecreaseOrderBinarySearch(int[] arrInt, int key)
+        {
+            // xuat ket qua
+            int left = 0;
+            int right = arrInt.Length - 1;
+            int mid;
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+                if (arrInt[mid] == key)
+                    return mid;
+                else if (arrInt[mid] < key)
+                    left = mid + 1;
+                else right = mid - 1;
+            }
+            return -1;
         }
 
         public static void Xuat(int[] arr)
