ee60365b75128a4e6184a60633b0cfa7190723f8
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-04-13 22:35:08 +0700

    new class điểm trung bình

---
+++ src/BangDiem/DiemTrungBinh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTLT2_TAODOITUONG.src.BangDiem
+{
+    class DiemTrungBinh
+    {
+        private double dTB;
+        public string KetQuaXepLoai(double dTB)
+        {
+            string ketqua = "";
+            if (dTB >= 0 && dTB < 5)
+            {
+                ketqua = "yeu";
+            }
+            else if (dTB >= 5 && dTB < 7)
+            {
+                ketqua = "Trung binh";
+            }
+            else if (dTB >= 7 && dTB < 8)
+            {
+                ketqua = "Kha";
+            }
+            else if (dTB >= 8 && dTB <= 10)
+            {
+                ketqua = "Gioi";
+            }
+
+            return ketqua;
+        }
+    }
+}
